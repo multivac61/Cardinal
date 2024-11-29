71826e0b4aa0c734034553cc18696ea246b9f2fe
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-11-29 07:00:03 +0700

    Init Submarine plugin

--- plugins/plugins.cpp
+++ plugins/plugins.cpp
@@ -977,6 +977,9 @@ StoermelderSettings pluginSettings;
 void StoermelderSettings::saveToJson() {}
 void StoermelderSettings::readFromJson() {}
 
+// Submarine
+#include "Submarine/src/SubmarineFree.hpp"
+
 // surgext
 #include "surgext/src/SurgeXT.h"
 void surgext_rack_initialize();
@@ -1121,6 +1124,7 @@ Plugin* pluginInstance__sonusmodular;
 Plugin* pluginInstance__StarlingVia;
 Plugin* pluginInstance__stocaudio;
 extern Plugin* pluginInstance__stoermelder_p1;
+Plugin* pluginInstance__Submarine;
 Plugin* pluginInstance__surgext;
 Plugin* pluginInstance__unless_modules;
 Plugin* pluginInstance__ValleyAudio;
@@ -3722,6 +3726,112 @@ static void initStatic__stoermelder_p1()
     }
 }
 
+static void initStatic__Submarine()
+{
+    Plugin* const p = new Plugin;
+    pluginInstance__Submarine = p;
+
+    const StaticPluginLoader spl(p, "Submarine");
+    if (spl.ok())
+    {
+      p->addModel(modelXX219);
+
+      p->addModel(modelAG104);
+      p->addModel(modelAG106);
+      p->addModel(modelAG202);
+      p->addModel(modelAO101);
+      p->addModel(modelAO106);
+      p->addModel(modelAO112);
+      p->addModel(modelAO118);
+      p->addModel(modelAO124);
+      p->addModel(modelAO136);
+      p->addModel(modelBB120);
+      p->addModel(modelDN112);
+      p->addModel(modelDO105);
+      p->addModel(modelDO110);
+      p->addModel(modelDO115);
+      p->addModel(modelDO120);
+      p->addModel(modelEN104);
+      p->addModel(modelEO102);
+      p->addModel(modelFF110);
+      p->addModel(modelFF120);
+      p->addModel(modelFF206);
+      p->addModel(modelFF212);
+      p->addModel(modelHS101);
+      p->addModel(modelLA108);
+      p->addModel(modelLA216);
+      p->addModel(modelLD103);
+      p->addModel(modelLD106);
+      p->addModel(modelLT116);
+      p->addModel(modelMZ909);
+      p->addModel(modelNG106);
+      p->addModel(modelNG112);
+      p->addModel(modelNG206);
+      p->addModel(modelOA103);
+      p->addModel(modelOA105);
+      p->addModel(modelOG104);
+      p->addModel(modelOG106);
+      p->addModel(modelOG202);
+      p->addModel(modelPG104);
+      p->addModel(modelPG112);
+      p->addModel(modelPO101);
+      p->addModel(modelPO102);
+      p->addModel(modelPO204);
+
+      p->addModel(modelSN101);
+
+      p->addModel(modelSS112);
+      p->addModel(modelSS208);
+      p->addModel(modelSS212);
+      p->addModel(modelSS220);
+      p->addModel(modelSS221);
+
+      p->addModel(modelTD116);
+      p->addModel(modelTD202);
+      p->addModel(modelTD316);
+      p->addModel(modelTD410);
+      p->addModel(modelTD510);
+      p->addModel(modelTF101);
+      p->addModel(modelTF102);
+      p->addModel(modelTM105);
+
+      p->addModel(modelVM101);
+      p->addModel(modelVM102);
+      p->addModel(modelVM104);
+      p->addModel(modelVM201);
+      p->addModel(modelVM202);
+      p->addModel(modelVM204);
+
+      p->addModel(modelWK101);
+      p->addModel(modelWK205);
+
+      p->addModel(modelWM101);
+      p->addModel(modelWM102);
+
+      p->addModel(modelXF101);
+      p->addModel(modelXF102);
+      p->addModel(modelXF104);
+      p->addModel(modelXF201);
+      p->addModel(modelXF202);
+      p->addModel(modelXF301);
+
+      p->addModel(modelXG104);
+      p->addModel(modelXG106);
+      p->addModel(modelXG202);
+
+      p->addModel(modelBP101);
+      p->addModel(modelBP102);
+      p->addModel(modelBP104);
+      p->addModel(modelBP108);
+      p->addModel(modelBP110);
+      p->addModel(modelBP112);
+      p->addModel(modelBP116);
+      p->addModel(modelBP120);
+      p->addModel(modelBP124);
+      p->addModel(modelBP132);
+    }
+}
+
 static void initStatic__surgext()
 {
     Plugin* const p = new Plugin;
@@ -4073,6 +4183,7 @@ void initStaticPlugins()
     initStatic__StarlingVia();
     initStatic__stocaudio();
     initStatic__stoermelder_p1();
+    initStatic__Submarine();
     initStatic__surgext();
     initStatic__unless_modules();
     initStatic__ValleyAudio();
