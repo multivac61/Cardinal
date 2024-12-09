5484cc4e9990a58a49561f9bd153b05b89f96333
Don Cross <cosinekitty@users.noreply.github.com>
2024-12-09 15:08:20 -0500

    Sapphire v 2.5.5: new module Chaops. (#770)
    Chaops is a left-expander for chaos modules Frolic, Glee, and Lark
    that adds new functionality to them.
    
    Improved CPU efficiency of Frolic, Glee, Lark by 30x in Turbo Mode.
    No longer need a CPU usage warning.
    
    Display chaos mode on top of CHOAS knob in Glee and Lark.
    Display "T" on top of SPEED knob when Turbo Mode is enabled.

--- plugins/plugins.cpp
+++ plugins/plugins.cpp
@@ -3610,6 +3610,7 @@ static void initStatic__Sapphire()
     const StaticPluginLoader spl(p, "Sapphire");
     if (spl.ok())
     {
+        p->addModel(modelSapphireChaops);
         p->addModel(modelSapphireElastika);
         p->addModel(modelSapphireFrolic);
         p->addModel(modelSapphireGalaxy);
